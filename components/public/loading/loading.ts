c3fa00f62f3cb3db8eba4d77e583f55dc9358304
panjiajie <251907882@qq.com>
2023-01-30 22:21:26 +0800

    进度

--- components/public/loading/loading.ts
+++ components/public/loading/loading.ts
@@ -1,9 +1,37 @@
 
 Component({
   properties: {
-
+    progress: {
+      value: 0,
+      type: Number
+    }
   },
   data: {
+    imageCompleteProgress: 0,
+    clipPath: ''
+  },
+  methods: {
+    complete(progress = 0): string {
+      const completeProgress: number = progress * 100;
+      const clipPath: string = `inset(0 0 ${completeProgress}%  0);`
+      return clipPath;
+    }
+  },
+  lifetimes: {
+    attached() {
+      this.setData({
+        clipPath: this.complete(this.data.imageCompleteProgress)
+      })
 
+    }
+  },
+  observers: {
+    progress(progress) {
+      if (progress >= 1) {
+        return
+      }
+      this.complete(progress)
+      this.data.completeProgress = this.properties.progress;
+    }
   }
 })
