55cb827a04f640323d306e8bdec3f56bf461845a
panjiajie <251907882@qq.com>
2023-02-08 17:53:44 +0800

    style->样式的变量

--- components/public/loading/loading.ts
+++ components/public/loading/loading.ts
@@ -1,40 +1,38 @@
-
 Component({
   properties: {
     progress: {
       value: 0,
-      type: Number
-    }
+      type: Number,
+    },
   },
   data: {
     imageCompleteProgress: 0,
-    clipPath: ''
+    clipPath: "",
   },
   methods: {
     complete(progress = 0): string {
       const completeProgress: number = progress * 100;
-      const clipPath: string = `inset(0 0 ${completeProgress}%  0);`
+      const clipPath: string = `inset(0 0 ${completeProgress}%  0);`;
       return clipPath;
-    }
+    },
   },
   lifetimes: {
     attached() {
       this.setData({
-        clipPath: this.complete(this.data.imageCompleteProgress)
-      })
-    }
+        clipPath: this.complete(this.data.imageCompleteProgress),
+      });
+    },
   },
   observers: {
     progress(progress) {
-      console.log(progress)
       if (progress >= 1) {
-        this.triggerEvent("complete");
-        return
+        // this.triggerEvent("complete");
+        return;
       }
       this.setData({
         clipPath: this.complete(progress),
-        imageCompleteProgress: progress
-      })
-    }
-  }
-})
+        imageCompleteProgress: progress,
+      });
+    },
+  },
+});
